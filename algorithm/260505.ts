d201b7962122e77c3ba1f058ef1da98bc33ed3d3
Konit611 <konit611@gmail.com>
2026-05-05 08:44:16 +0900

    TIL 20260505 - 2

--- algorithm/260505.ts
+++ algorithm/260505.ts
@@ -16,6 +16,24 @@ function findErrorNums(nums: number[]): number[] {
     return answer;
 };
 
+function findErrorNumsBetter(nums: number[]): number[] {
+    const counts = new Array(nums.length).fill(0);
+    const result: [number, number] = [0, 0]; // [중복값, 누락값]
+
+    for (const num of nums) {
+        counts[num - 1]++;
+    }
+
+    for (let i = 0; i < counts.length; i++) {
+        if (counts[i] === 2) result[0] = i + 1;
+        else if (counts[i] === 0) result[1] = i + 1;
+        
+        if (result[0] !== 0 && result[1] !== 0) break;
+    }
+
+    return result;
+}
+
 let nums = [1,2,2,4];
 let nums_1 = [1,1];
 let nums_2 = [2,2];
